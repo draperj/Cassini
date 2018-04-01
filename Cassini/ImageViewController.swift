762715d569a3d0f632c25257a3ac07233c80f9e2
Joshua Draper <draperj@nyc-p011356-m.home>
2018-04-01 18:14:26 -0400

    Lecture 9 commit, fixing VC

--- Cassini/ImageViewController.swift
+++ Cassini/ImageViewController.swift
@@ -8,28 +8,64 @@
 
 import UIKit
 
-class ImageViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+class ImageViewController: UIViewController, UIScrollViewDelegate
+{
+    var imageURL: URL? {
+        didSet {
+            image = nil
+            if view.window != nil {
+                fetchImage()
+            }
+        }
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    private var image: UIImage? {
+        get {
+            return imageView.image
+        }
+        set {
+            imageView.image = newValue
+            imageView.sizeToFit()
+            scrollView?.contentSize = imageView.frame.size
+        }
     }
-    */
-
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        if imageView.image == nil {
+            fetchImage()
+        }
+    }
+    
+    @IBOutlet weak var scrollView: UIScrollView! {
+        didSet {
+            scrollView.minimumZoomScale = 1/25
+            scrollView.maximumZoomScale = 1.0
+            scrollView.delegate = self
+            scrollView.addSubview(imageView)
+        }
+    }
+    
+    func viewForZooming(in scrollView: UIScrollView) -> UIView? {
+        return imageView
+    }
+    
+    var imageView  = UIImageView()
+    
+    private func fetchImage() {
+        if let url = imageURL {
+            let urlContents = try? Data(contentsOf: url)
+            if let imageData = urlContents {
+                self.image = UIImage(data: imageData)
+            }
+        }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        if imageURL == nil {
+            imageURL = DemoURLs.stanford
+        }
+    }
 }
+
